967e8318ef50ce4ad0c709cb5bbdc9af92899538
lypant <lypant@tlen.pl>
2017-04-23 14:49:13 +0200

    Initial versions of logger and bashcommand

---
+++ bashcommand.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python2
+
+from __future__ import print_function
+
+import subprocess
+import select
+import os
+import logger
+
+class BashCommand():
+    READ_SIZE = 1024
+
+    class ExecutionError(Exception):
+        '''
+        For more convenient analysis of bash command execution errors
+        '''
+        def __init__(self, command, exitCode, stdErr):
+            '''
+            @param command bash command which caused an execution error
+            @param exitCode exit code returned by bash when executing the command
+            @param stdErr output passed to stderr by the command
+            '''
+            Exception.__init__(self)
+            self.command = command
+            self.exitCode = exitCode
+            self.stdErr = stdErr
+
+        def __str__(self):
+            return '\n\tBash command: %s\n\tExit code: %s\n\tStd err: %s' % \
+                    (self.command, self.exitCode, self.stdErr)
+
+    def __init__(self, command, description='', input=''):
+        self.command = command
+        self.description = description
+        self.input = input
+
+        self._cleanupTemporaryData()
+
+    def _cleanupTemporaryData(self):
+        self.logger = None
+        self.inputLines = []
+        self.process = None
+        self.inputStreams = []
+        self.outputStreams = []
+        self.errMsg = ''
+
+    def execute(self, logger=None):
+        # Set logger
+        self.logger = logger
+
+        self._createSubprocess()
+        self._prepareInputStreamsAndData()
+        self._prepareOutputStreams()
+        self._waitForIOCompletion()
+        self.process.wait()
+        self._checkReturnCode()
+
+        self._cleanupTemporaryData()
+
+    def _createSubprocess(self):
+        self.process =  subprocess.Popen(
+            ['/usr/bin/bash', '-c', self.command],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+
+    def _prepareInputStreamsAndData(self):
+        if self.input:
+            self.inputLines = [
+                line + '\n' for line in input.rstrip().split('\n')]
+            self.inputStreams = [self.process.stdin.fileno()]
+
+    def _prepareOutputStreams(self):
+        self.outputStreams = [
+            self.process.stdout.fileno(),
+            self.process.stderr.fileno()]
+
+    def _passInputLinesToSubprocess(self):
+        for line in self.inputLines:
+            os.write(self.inputStreams[0], line)
+            self.inputLines.remove(line)
+
+    def _storeMessageWhenErrorStream(self, fileDescriptor, data):
+        if fileDescriptor == self.process.stderr.fileno():
+            self.errMsg += data
+
+    def _logOutput(self, data):
+        if self.logger:
+            self.logger.logOutput(data)
+
+    def _handleSubprocessOutput(self):
+        for fileDescriptor in self.outputStreams:
+            data = os.read(fileDescriptor, BashCommand.READ_SIZE)
+            if data:
+                self._storeMessageWhenErrorStream(fileDescriptor, data)
+                self._logOutput(data.rstrip())
+            else:
+                self.outputStreams.remove(fileDescriptor)
+
+    def _waitForIOCompletion(self):
+        while self.outputStreams:
+            r, w, x = select.select(self.outputStreams, self.inputStreams, [])
+            self._passInputLinesToSubprocess()
+            self._handleSubprocessOutput()
+
+    def _checkReturnCode(self):
+        if self.process.returncode:
+            raise BashCommand.ExecutionError(
+                self.command,
+                self.process.returncode,
+                self.errMsg)
+
+
+if __name__ == '__main__':
+    lgr = logger.Logger('/tmp/bashcommand.log')
+    # bc1 = BashCommand('ls /')
+    # bc2 = BashCommand('ls /home')
+    # bc1.execute(lgr)
+    # bc2.execute(lgr)
+    # bc1.execute()
+    # bc1.execute(lgr)
+    # bc1.execute(lgr)
+
+    bc3 = BashCommand('ls /zonk')
+try:
+    bc3.execute(lgr)
+except BashCommand.ExecutionError as e:
+     lgr.logError(e)
---
+++ logger.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python2
+
+from __future__ import print_function
+import logging
+import logging.config
+
+
+class Logger():
+    '''
+    Provides console and file logging services.
+    Each log entry is passed to both console and file handlers.
+    Filtering based on log levels for each of the handlers is done.
+    The console handler by default is configured to show higher level output
+    with progress tracking purposes.
+    The file handler by default is configured to show same contents as
+    the console handler plus additional levels with exact bash command text and
+    its output
+    '''
+    # Aliases to logging levels to match project context
+    # Top to bottom - highest to lowest priority
+    ERROR = logging.CRITICAL
+    STATISTIC = logging.ERROR
+    COMMAND = logging.WARNING
+    OUTPUT = logging.INFO
+    # <RESERVED> = logging.DEBUG # not used at the moment
+
+    def __init__(self,
+                 logFile,
+                 fileLogLevel=OUTPUT,
+                 fileMode='w',
+                 consoleLogLevel=STATISTIC,
+                 format='<%(asctime)s> %(message)s',
+                 datefmt=''):
+        '''
+        @param logFile path and name of a log file to be created
+        @param fileLogLevel lowest level of logs stored in the log file
+        @param fileMode file log open mode - write (default) or append
+        @param consoleLogLevel lowest level of logs printed to console
+        @param format format of each log entry
+        @param datefmt format of timestamp used for each log entry
+        '''
+        dictConf = dict(
+            version=1,
+            formatters={
+                'myFormatter': {
+                    'format': format,
+                    'datefmt': datefmt}},
+            handlers={
+                'myConsoleHandler': {
+                    'class': 'logging.StreamHandler',
+                    'formatter': 'myFormatter',
+                    'level': consoleLogLevel},
+                'myFileHandler': {
+                    'class': 'logging.FileHandler',
+                    'formatter': 'myFormatter',
+                    'level': fileLogLevel,
+                    'filename': logFile,
+                    'mode': fileMode}},
+            loggers={
+                'myLogger': {
+                    'handlers': ['myConsoleHandler', 'myFileHandler'],
+                    'level': Logger.OUTPUT}})
+        logging.config.dictConfig(dictConf)
+        self.logger = logging.getLogger('myLogger')
+
+    def logError(self, msg, *args, **kwargs):
+        '''
+        Main log level - use to show currently executed command and progress
+        @param msg text to be logged
+        @param args additional parameters to pass to logging.logger
+        @param kwargs additional parameters to pass to logging.logger
+        '''
+        self.logger.critical(msg, *args, **kwargs)
+
+    def logStatistic(self, msg, *args, **kwargs):
+        '''
+        Main log level - use to show currently executed command and progress
+        @param msg text to be logged
+        @param args additional parameters to pass to logging.logger
+        @param kwargs additional parameters to pass to logging.logger
+        '''
+        self.logger.error(msg, *args, **kwargs)
+
+    def logCommand(self, msg, *args, **kwargs):
+        '''
+        Middle log level - use to show details of bash command to be executed
+        @param msg text to be logged
+        @param args additional parameters to pass to logging.logger
+        @param kwargs additional parameters to pass to logging.logger
+        '''
+        self.logger.warning(msg, *args, **kwargs)
+
+    def logOutput(self, msg, *args, **kwargs):
+        '''
+        Low log level - use to show output of the executed bash command
+        @param msg text to be logged
+        @param args additional parameters to pass to logging.logger
+        @param kwargs additional parameters to pass to logging.logger
+        '''
+        self.logger.info(msg, *args, **kwargs)
+
+    def logException(self, msg, *args, **kwargs):
+        '''
+        Use in exception handler to provide more data about bash command error
+        @param msg text to be logged
+        @param args additional parameters to pass to logging.logger
+        @param kwargs additional parameters to pass to logging.logger
+
+        '''
+        self.logger.exception(msg, *args, **kwargs)
+
+if __name__ == '__main__':
+    log = '/tmp/bcf_logger.log'
+    print('(check %s for file handler output)' % log)
+    logger = Logger(log)
+    logger.logStatistic('List root directory...')
+    logger.logCommand('ls -l /')
+    logger.logOutput('one\ntwo\nthree')
+    logger.logStatistic('List root directory...done')
